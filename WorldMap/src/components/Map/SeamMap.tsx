3de17ee3101771dca82b5d951567e0467ae7249a
Igal Vinogradov <igalv6@gmail.com>
2020-01-13 13:55:26 +0200

    Implement context menu for the map

--- WorldMap/src/components/Map/SeamMap.tsx
+++ WorldMap/src/components/Map/SeamMap.tsx
@@ -2,12 +2,51 @@ import React from 'react';
 import styles from './SeamMap.module.css';
 import Box from '@material-ui/core/Box';
 import PerformanceMap from "./PerformanceMap";
+import { MenuProvider, Menu, Item, Separator, Submenu, theme, animation } from 'react-contexify';
+import { makeStyles, Theme, createStyles } from '@material-ui/core';
+import 'react-contexify/dist/ReactContexify.min.css';
 
-export class SeamMap extends React.Component {
+// create your menu first
+const MapContextMenu = () => (
+    <Menu id='menu_id' theme={theme.dark}>
+        <Item onClick={() => { }}>הוסף ישות</Item>
+        <Separator />
+        <Item onClick={() => { }}>פתח סרגל</Item>
+        <Separator />
+        <Item onClick={() => { }}>הפעל את כל הישויות</Item>
+        <Separator />
+        <Item onClick={() => { }}>הסתר את כל הגילויים</Item>
+    </Menu>
+);
 
-    render() {
-        return (<Box className={styles.SeamMap}>
-            <PerformanceMap/>
-        </Box>);
-    }
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        map: {
+            opacity: 1 //for future custom design
+        },
+        contextMenu: {
+            width: "100vw",
+            height: "100vh",
+            position: "absolute",
+            backgroundColor: 'blue',
+            opacity: 0.9999999
+        },
+        openedContextMenu: {
+            textAlign: "right"
+        },
+    })
+);
+
+
+export function SeamMap() {
+    const classes = useStyles();
+
+    return (<Box>
+        <MenuProvider id="menu_id" className={classes.contextMenu}>
+            <Box className={classes.map}>
+                <PerformanceMap />
+            </Box>
+        </MenuProvider>
+        < MapContextMenu />
+    </Box >);
 }
